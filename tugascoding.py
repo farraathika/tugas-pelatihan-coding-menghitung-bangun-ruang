840a85b9bc091423a40d51133dcad2bd283381b4
farraathika <113606626+farraathika@users.noreply.github.com>
2022-09-15 23:43:59 +0700

    Add files via upload

---
+++ tugascoding.py
@@ -0,0 +1,13 @@
+# 1. volume kubus
+print("1. menghitung volume kubus")
+s = int(input('sisi:'))
+v = s*s*s
+print("jika sisi kubus adalah",s,"cm,","maka volume kubus adalah",v,"cm3.")
+
+# 2. volume balok
+print("2. menghitung volume balok")
+p = int(input('panjang balok:'))
+l = int(input('lebar balok:'))
+t = int(input('tinggi balok:'))
+v = p*l*t
+print("jika panjang balok",p,"cm,","lebar balok",l,"cm","tinggi balok",t,"cm","maka volume balok adalah",v,"cm3.")
